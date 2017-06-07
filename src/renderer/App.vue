06ccf0a58f7b9302dbba5557f7d0b995c5524c68
Walee Ahmed <walee.tausif@gmail.com>
2017-06-07 11:42:13 -0400

    Added basic project structure

---
+++ src/renderer/App.vue
@@ -0,0 +1,17 @@
+<template lang="pug">
+  #app
+    router-view
+</template>
+
+<script>
+  export default {
+    name: 'zapper'
+  }
+</script>
+
+<style lang="stylus" scoped>
+  @import url('https://fonts.googleapis.com/icon?family=Material+Icons')
+  @import url('http://cdn.materialdesignicons.com/1.9.32/css/materialdesignicons.css')
+  @import url('https://fonts.googleapis.com/css?family=Roboto')
+  @import '~vuetify/src/stylus/main'
+</style>
