bd1ccd5b72aea8c0755621f86e881f66f02b6fbc
Walee Ahmed <walee.tausif@gmail.com>
2017-06-07 22:27:53 -0400

    Added default layout with related route setup

---
+++ src/renderer/pages/NotFound.vue
@@ -0,0 +1,13 @@
+<template lang="pug">
+  .not-found-page
+    h1 Page not found!
+</template>
+
+<script>
+  export default {}
+</script>
+
+<style lang="stylus" scoped="">
+  .not-found-page
+    /**/
+</style>
--- src/renderer/router/index.js
+++ src/renderer/router/index.js
@@ -1,18 +1,31 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 
+import { DefaultLayout } from '../layouts'
+import {
+  HomePage,
+  NotFound
+} from '../pages'
+
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
       path: '/',
-      name: 'landing-page',
-      component: require('@/components/LandingPage')
-    },
-    {
-      path: '*',
-      redirect: '/'
+      component: DefaultLayout,
+      children: [
+        {
+          path: '',
+          name: 'HomePage',
+          component: HomePage
+        },
+        {
+          path: '*',
+          name: '404',
+          component: NotFound
+        }
+      ]
     }
   ]
 })
