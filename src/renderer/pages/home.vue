621afbc225bfea1a5ac9a806783ec1ac076d3152
Walee Ahmed <walee.tausif@gmail.com>
2017-06-08 08:32:38 -0400

    achived simple connection reddit api setup

--- src/renderer/pages/home.vue
+++ src/renderer/pages/home.vue
@@ -4,6 +4,17 @@
 </template>
 
 <script>
+  import Snoowrap from 'snoowrap'
+
+  /* eslint-disable no-new */
+  const reddit = new Snoowrap({
+    userAgent: 'Zapper Web'
+  })
+  reddit.getUser('not_an_aardvark').fetch().then(userInfo => {
+    console.log(userInfo.name) // 'not_an_aardvark'
+    console.log(userInfo.created_utc) // 1419104352
+  })
+//  reddit.getHot().map(post => post.title).then(console.log)
   export default {
     name: 'HomePage'
   }
